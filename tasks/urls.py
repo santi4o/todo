693032f4fec62b2db01114a83065443f2be350e8
santi4o <yinyue.sant@gmail.com>
2021-04-20 14:22:51 -0500

    tasks are now editable

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
+app_name = 'tasks'
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='list'),
+    path('update_task/<str:pk>/', views.updateTask, name='update_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
+from django.urls import reverse
 from .models import *
 from .forms import *
 
@@ -20,3 +21,19 @@ def index(request):
 
     context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/list.html', context)
+
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+
+    form = TaskForm(instance=task)
+
+    if request.method=='POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(reverse('tasks:list'))
+
+    context = {'form': form}
+
+    return render(request, 'tasks/update_task.html', context)
